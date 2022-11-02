e849ea2f6c589155e7f211589bc4770d0c3b74fa
terassyi <iscale821@gmail.com>
2022-11-02 18:25:22 +0900

    wip

--- sartd/test/simple/gobgp_run.sh
+++ sartd/test/simple/gobgp_run.sh
@@ -2,9 +2,9 @@
 set -x
 set -e
 
-sudo ip netns exec spine1 gobgpd -f sartd/testdata/simple/gobgp_spine1.conf &
-sudo ip netns exec spine2 gobgpd -f sartd/testdata/simple/gobgp_spine2.conf &
+sudo ip netns exec spine1 gobgpd -f sartd/test/simple/gobgp_spine1.conf &
+sudo ip netns exec spine2 gobgpd -f sartd/test/simple/gobgp_spine2.conf &
 
-sleep 3
+sleep 1
 sudo ip netns exec spine1 gobgp neighbor add 10.0.0.1 as 100
 sudo ip netns exec spine2 gobgp neighbor add 10.0.1.1 as 100
