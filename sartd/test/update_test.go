9fee1fb2a576dc8688abc7a9b1e7a6ef82e76e1b
terasihma <tomoya-terashima@cybozu.co.jp>
2023-02-07 07:10:33 +0000

    handle initial path

--- sartd/test/update_test.go
+++ sartd/test/update_test.go
@@ -456,4 +456,89 @@ func testUpdate() {
 		err = cmd.Run()
 		Expect(err).NotTo(HaveOccurred())
 	})
+
+	It("should send update messages to advertise initially installed paths", func() {
+		ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+
+		By("preparing the topology")
+		cmd := exec.Command("./simple_rib/topology.sh")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		Expect(err).NotTo(HaveOccurred())
+
+		By("starting frr daemon for gobgp")
+		cmd = exec.Command("sudo", "./simple_rib/frr_run.sh")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+		// Expect(err).NotTo(HaveOccurred())
+
+		By("starting gobgp daemon in netns")
+		go func(context.Context) {
+			_, _, _, err = execInNetns("spine1", "gobgpd", "-f", "simple_rib/gobgp_spine1.conf")
+			Expect(err).NotTo(HaveOccurred())
+		}(ctx)
+
+		go func(context.Context) {
+			_, _, _, err = execInNetns("spine2", "gobgpd", "-f", "simple_rib/gobgp_spine2.conf")
+			Expect(err).NotTo(HaveOccurred())
+		}(ctx)
+
+		time.Sleep(time.Second)
+
+		By("checking configurations")
+		Eventually(func(g Gomega) error {
+			if err := checkGobgpConfig("spine1", 100); err != nil {
+				return err
+			}
+			if err := checkGobgpConfig("spine2", 100); err != nil {
+				return err
+			}
+			return nil
+		}, "1m").Should(Succeed())
+
+		By("starting sartd-bgp")
+		go func(context.Context) {
+			_, _, _, err := execInNetns("core", "../target/debug/sartd", "-f", "simple/config_init_path.yaml")
+			Expect(err).NotTo(HaveOccurred())
+		}(ctx)
+
+		By("checking to establish peers")
+		Eventually(func(g Gomega) error {
+			if err := checkGobgpConfig("spine1", 100); err != nil {
+				return err
+			}
+			if err := checkGobgpConfig("spine2", 100); err != nil {
+				return err
+			}
+			return nil
+		}, "1m").Should(Succeed())
+
+		By("checking spine1 and spine2 received paths advertised by core(sartd-bgp)")
+		out, _, _, err := execInNetns("spine1", "gobgp", "global", "rib", "-a", "ipv4", "-j")
+		var res map[string]any
+		err = json.Unmarshal(out, &res)
+		Expect(err).NotTo(HaveOccurred())
+		pref1 := res["10.1.0.0/24"].([]any)
+		Expect(len(pref1)).To(Equal(1))
+		pref2 := res["10.2.0.0/24"].([]any)
+		Expect(len(pref2)).To(Equal(1))
+		pref3 := res["192.168.0.10/32"].([]any)
+		Expect(len(pref3)).To(Equal(1))
+
+		By("stopping frr daemon for gobgp")
+		cmd = exec.Command("sudo", "./simple_rib/frr_stop.sh")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		// Expect(err).NotTo(HaveOccurred())
+
+		By("cleaning up the topology")
+		cmd = exec.Command("./simple/clean_topology.sh")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		Expect(err).NotTo(HaveOccurred())
+	})
 }
